2f56e1459598dc7c2b8e18a5ce4fed1be9d51f84
arpithasureshappa <arpitha.sureshappa@tarento.com>
2024-12-19 11:50:40 +0530

    auth token dynamic creation implemented

--- src/main/java/com/igot/service_locator/plugins/ContentPartnerPluginService.java
+++ src/main/java/com/igot/service_locator/plugins/ContentPartnerPluginService.java
@@ -2,8 +2,9 @@ package com.igot.service_locator.plugins;
 
 
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.stereotype.Service;
 @Service
 public interface ContentPartnerPluginService {
-    String generateAuthHeader();
+    String generateAuthHeader(JsonNode jsonNode);
 }
